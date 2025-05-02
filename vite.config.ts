5d262b0525a512f5827cd5f55ab0dacd440c342c
Ryan Fitzgerald <ryfitz@ymail.com>
2025-05-01 22:10:18 -0700

    changed build setting in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    // outDir: 'dist', // This is the default, you can keep it or change it
+  },
+  base: './', // Important for correct asset paths on GitHub Pages
 });
